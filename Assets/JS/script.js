1a51ea0dd422846d5602333c0067c6499689deb5
murielledouay <guyard_murielle@orange.fr>
2020-05-04 16:58:41 +0200

    JS PARTIE 2 EXO 3

---
+++ Assets/JS/script.js
@@ -0,0 +1,14 @@
+  function reduce(){
+    var element = document.getElementById('greenCar');
+    element.style.height = '50%';
+    element.style.width = '50%';
+  }
+
+
+
+/*en CSS :
+img{
+  height : 100%;
+  width : 100%;
+}
+*/
